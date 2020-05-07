8ff0e79dffb105d5e1ca0fd6810ef071cb662c9c
Belinda Brown <belindabrownr04@gmail.com>
2020-05-06 19:52:06 -0600

    English

--- Components_Logic_Gates/mux2_1_2bit_tester.v
+++ Components_Logic_Gates/mux2_1_2bit_tester.v
@@ -1,4 +1,4 @@
-// Se define el modulo tester para mux2_1_2bit
+// The tester module is defined for mux2_1_2bit
 
 module mux2_1_2bit_tester(
 output reg [1:0] in1_mux2bits_tester,
@@ -15,7 +15,7 @@ begin
 in1_mux2bits_tester = 2'b0;
 in2_mux2bits_tester  =  2'b0;
 selec_mux2bits_tester =  2'b0;
-  // Declarando repeticiones
+// Declaring repetitions
 	in1_mux2bits_tester[0] <= #10 2'b0; in2_mux2bits_tester[0] <= #10 2'b0; selec_mux2bits_tester <= #10 1'b0;
 	in1_mux2bits_tester[0] <= #10 2'b0; in2_mux2bits_tester[0] <= #10 2'b0; selec_mux2bits_tester <= #10 1'b1;
 	in1_mux2bits_tester[0] <= #10 2'b0; in2_mux2bits_tester[0] <= #10 2'b1; selec_mux2bits_tester <= #10 1'b0;
