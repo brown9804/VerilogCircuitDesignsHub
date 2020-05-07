890260770b27caf0f1982245eeb9b9fe273c1793
Belinda Brown <belindabrownr04@gmail.com>
2020-05-07 01:11:09 -0600

    ready

--- Components_Logic_Gates/or_two_input_gate_tester.v
+++ Components_Logic_Gates/or_two_input_gate_tester.v
@@ -7,13 +7,24 @@ output reg in_or2_tester,
 input wire out_or_tester
 );
 
-// Starting the tester
+
+// Starting the tester for OR gate 
 initial
 begin
 	$dumpfile("or_two_input_gate_tester.vcd");
 	$dumpvars;
-in_or1_tester = 0;
-in_or2_tester =0;
+
+	in_or1_tester = 0;
+	in_or2_tester = 0;
+
+
+	#5 in_or1_tester = 1;
+	#5 in_or2_tester = 1;
+	
+	#5 in_or1_tester = 0;
+	#5 in_or2_tester = 0;
+
+
 // It is defined how many times the tasks are performed
 	in_or1_tester <= #15 0;  in_or2_tester <= #15 0;
 	in_or1_tester <= #15 0;  in_or2_tester <= #15 1;
