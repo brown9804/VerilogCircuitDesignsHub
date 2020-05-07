a32e5313dbe522a709a60d8ebf399dbf881f1357
Belinda Brown <belindabrownr04@gmail.com>
2020-05-06 19:45:32 -0600

    English

--- Components_Logic_Gates/flip_flop_single_type_D_tester.v
+++ Components_Logic_Gates/flip_flop_single_type_D_tester.v
@@ -1,29 +1,29 @@
-// Tester para flip_flop_single_type_D.v
+// tester for flip_flop_single_type_D.v
 
-// Definicion del modulo
+// Module definition
 module flip_flop_single_type_D_tester(
 output reg clk_tester,
 output reg D_tester,
 input wire Q_tester
 );
 
-// Iniciando las pruebas
+// Starting the tests
 initial
 begin
 	$dumpfile("flip_flop_single_type_D_tester.vcd");
 
-// Mensaje que se imprime en consola una vez
+// Message that prints to console once
 	$display ("\t\t\tclk_tester, \tQ_tester , \tD_tester");
 
-// Mensaje que se imprime en consola cada vez que un elemento de la lista cambia
+// Message that is printed in console every time a list item changes
 	$monitor($time,"\t%b\t%b\t\t%b", clk_tester, Q_tester, D_tester);
 
-	// Se utiliza para especificar qué variables se van a poner en el dump file. La bibliografía menciona que la manera
-	// simple de realizarlo es solo con $dumpvars dado que de este modo las asigan todas al banco de pruebas
+// It is used to specify which variables are to be put in the dump file. The bibliography mentions that the way
+// simple to do it is only with $ dumpvars since this way they all assign them to the test bench
 	$dumpvars;
 D_tester = 0;
 
-	// Se estipula la cantidad de repitaciones estas son las que van #
+// The number of repetitions is stipulated, these are the ones that go #
   D_tester <= #10 1; clk_tester <= #6 1;
 	D_tester <= #5 0;
 	D_tester <= #10 0; clk_tester <= #6 0;
@@ -32,7 +32,7 @@ D_tester = 0;
 	D_tester <= #5 0 ;
 	D_tester <= #10 1; 	clk_tester <= #6 0;
 	D_tester <= #5 0;
-  // Se imprime diez veces
+// prints ten times
 	#40 $finish;
 end // end initial
 endmodule
