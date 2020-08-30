5effc2ee8d0f1ad8926d8a59c2ff3d574478c547
Belinda Brown <belindabrownr04@gmail.com>
2020-08-30 00:35:44 -0600

    Delete inversor_as_not_gate.v

--- Multiplexor21_2bits_Logic_Gates/inversor_as_not_gate.v
+++
@@ -1,8 +0,0 @@
-module inversor_as_not_gate(
-// Both output and input are wires
-input in_not,
-output out_not
-);
-      // Assign the inverse input to the output
-      assign #2.4 out_not = ~in_not;
-    endmodule
